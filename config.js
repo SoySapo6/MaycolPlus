6b74599e842c82c6637c02242ed0ba88baf3df1a
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-10-15 21:27:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ global.owner = [['180650938249287', 'SoyMaycol', true]]
 global.mods = []
 global.prems = []
 
+global.emoji = '🍡'
 global.namebot = '𝙼𝚊𝚢𝚌𝚘𝚕𝐏𝐥𝐮𝐬'
 global.redes = 'https://chat.whatsapp.com/KDI7NNovzdwJayx1gI1cue?mode=ems_copy_t'
 global.botname = '𝕄𝕒𝕪𝕔𝕠𝕝𝐏𝐥𝐮𝐬'
